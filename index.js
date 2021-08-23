adc307643ea537bb205a35570d5f33c0f898d1f2
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-08-23 15:53:20 -0500

    add change event listener dropdown

--- index.js
+++ index.js
@@ -20,8 +20,12 @@ getCharacters()
 function getCharacters(){
     fetch(charactersURL)
     .then(resp => resp.json())
-    .then(character => { appendCharacters(character) 
-        addCharacterOptions(character)
+    .then(character => { 
+        // console.log(character[1].name)
+        appendCharacters(character) 
+        addCharacterOptions1(character)
+        addCharacterOptions2(character)
+        changeNameEvent(character)
     })
 }
 
@@ -35,11 +39,41 @@ function appendCharacters(character){
     Character1.src = character.img
 }
 
-function addCharacterOptions(character) {
+const nameDropdown = document.querySelectorAll(".names")
+
+const nameDropdown1 = nameDropdown[0]
+const nameDropdown2 = nameDropdown[1]
+
+console.log(nameDropdown2)
+
+function addCharacterOptions1(character) {
     const removeWalterArray = character.slice(1)
     removeWalterArray.forEach(character => {
         const characterOptions = document.createElement('option')
         characterOptions.textContent = character.name
-        console.log(characterOptions)
+        nameDropdown1.append(characterOptions);
     })
 }
+
+function addCharacterOptions2(character) {
+    const removeWalterArray = character.slice(1)
+    removeWalterArray.forEach(character => {
+        const characterOptions = document.createElement('option')
+        characterOptions.textContent = character.name
+        nameDropdown2.append(characterOptions);
+    })
+}
+
+function changeNameEvent(character) {
+    nameDropdown1.addEventListener('change', (event) => {
+        const inputValue = nameDropdown1.value;
+        for(i = 0; i < character.length; i++){
+            if(character[i].name === inputValue){
+                console.log(character[i])
+            }
+        }
+    })
+}
+
+// change img src
+// add details to details div
